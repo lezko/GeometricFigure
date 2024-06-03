c58bbd5021631a7995e48eee190441acc385bcdc
Timophey <ulezko.2015@mail.ru>
2024-06-03 10:45:28 +0300

    Add project files.

---
+++ Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Line : GeometricFigure
+    {
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
+
+        public Line(double x, double y, double x2, double y2) : base(x, y)
+        {
+            X2 = x2;
+            Y2 = y2;
+        }
+
+        public override (double X1, double Y1, double X2, double Y2) GetBoundingRectangle()
+        {
+            return (Math.Min(X, X2), Math.Min(Y, Y2), Math.Max(X, X2), Math.Max(Y, Y2));
+        }
+
+        public override double GetArea()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            GeometricFigure point = new Point(1, 1);
+            GeometricFigure line = new Line(1, 1, 4, 4);
+            GeometricFigure polygon = new Polygon(0, 0, new List<(double X, double Y)> { (1, 1), (5, 1), (4, 5), (1, 4) });
+            GeometricFigure ellipse = new Ellipse(0, 0, 3, 2);
+
+            PrintFigureInfo(point);
+            PrintFigureInfo(line);
+            PrintFigureInfo(polygon);
+            PrintFigureInfo(ellipse);
+        }
+
+        public static void PrintFigureInfo(GeometricFigure figure)
+        {
+            var (x1, y1, x2, y2) = figure.GetBoundingRectangle();
+            Console.WriteLine($"Figure: {figure.GetType().Name}");
+            Console.WriteLine($"Bounding Rectangle: ({x1}, {y1}) to ({x2}, {y2})");
+            Console.WriteLine($"Area: {figure.GetArea()}");
+            Console.WriteLine();
+        }
+    }
+}
